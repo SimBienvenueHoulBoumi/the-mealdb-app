79b59c168f41a6f6b8e85b82fe3e3c687a94ce1a
Sim <brhulla@gmail.com>
2024-01-21 20:23:38 +0100

    setting up the infrastructure

---
+++ README.md
@@ -0,0 +1,10 @@
+# Description
+TheMealDb Api is an online restaurant order management application.
+
+# Tech
+- Nestjs : backend -> meal-api (mongoDB)
+- Angular : IHM admin -> meal-admin-app
+- Nextjs : Marketplace -> meal-marketplace-app
+
+# link
+- pdAdmin -> [http://localhost:8080]
---
+++ meal-db/initdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
+    CREATE SCHEMA IF NOT EXISTS public;
+    CREATE TABLE public.meals (
+        id SERIAL PRIMARY KEY,
+        mealID TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        unit_price FLOAT NOT NULL
+    );
+    CREATE ROLE noadmin NOLOGIN;
+    CREATE ROLE admin NOINHERIT LOGIN PASSWORD '$POSTGRES_PASSWORD';
+    GRANT noadmin TO admin;
+EOSQL
