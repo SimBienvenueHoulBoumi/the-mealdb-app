0613d7ce8f425824f5e73153d61f1708be2e5259
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-03-07 17:15:32 +0100

    update style

--- meal-ihm/src/app/auth/signin/signin.component.ts
+++ meal-ihm/src/app/auth/signin/signin.component.ts
@@ -1,20 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/authentication.service';
 import LoginDto from '../../../entities/login.dto';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
   providers: [AuthService],
-  imports: [RouterLink, RouterOutlet, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatProgressSpinnerModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
 })
 export class SigninComponent {
   authService = inject(AuthService);
   router = inject(Router);
+  isloading: boolean = false;
+
+  usernameIsValid: boolean = true;
+  passwordIsValid: boolean = true;
 
   ngOnInit(): void {
     if (typeof localStorage !== 'undefined') {
@@ -34,6 +40,23 @@ export class SigninComponent {
 
   loginForm(event: Event) {
     event.preventDefault();
-    this.authService.login(this.authForm.value as LoginDto, event);
+    
+    if(!this.authForm.value.password){
+      this.passwordIsValid = false;
+      return;
+    }
+    
+    if(!this.authForm.value.username){
+      this.usernameIsValid = false;
+      return;
+    }
+    
+    this.isloading = true;
+
+    // Utiliser setTimeout pour réinitialiser isloading à false après 5 secondes
+    setTimeout(() => {
+      this.authService.login(this.authForm.value as LoginDto, event);
+      this.isloading = false;
+    }, 5000);
   }
 }
